09b66e962ae4314c9042f1f26b38c3450f101244
monsterzack5 <monsterzack5@gmail.com>
2022-07-14 22:19:37 -0400

    Keyfob/Module: Implement Crypto+MessageParams
    Bring LoraModule up to speed with LoraRemote
    - Implement MessageParams
    - Remove Local crypto function
    - Use ZephyrAesGCM crypto functions

--- Modules/KeyFobProject/LoraModule/src/main.cpp
+++ Modules/KeyFobProject/LoraModule/src/main.cpp
@@ -1,18 +1,17 @@
 #include <AES_GCM_KEY.h>
 #include <KeyFobConfig.h>
+#include <ZephyrAesGCM.h>
+#include <crypto/cipher.h>
 #include <device.h>
+#include <drivers/flash.h>
 #include <drivers/gpio.h>
 #include <drivers/lora.h>
+#include <fs/nvs.h>
 #include <logging/log.h>
 #include <pm/pm.h>
+#include <storage/flash_map.h>
 #include <sys/util.h>
 #include <zephyr.h>
-
-#include <drivers/flash.h>
-#include <fs/nvs.h>
-#include <storage/flash_map.h>
-
-#include <crypto/cipher.h>
 
 // LoRa setup
 BUILD_ASSERT(DT_NODE_HAS_STATUS(DT_ALIAS(lora0), okay),
@@ -55,68 +54,133 @@ const device* start_car_device;
 const uint8_t start_car_pin = 10;
 // ---
 
-static RollingCode_t rolling_code = 0;
-static IV_t iv = 0;
-
-bool aes_gcm_decrypt(uint8_t* buffer, IV_t found_iv)
+void handle_command(Command command)
 {
-    cipher_ctx init;
+    const auto blink_pin = [](const device* device, uint8_t pin) {
+        gpio_pin_set(device, pin, 1);
+        k_sleep(K_MSEC(20));
+        gpio_pin_set(device, pin, 0);
+    };
 
-    init.keylen = 16;
-    // TODO: Why would this not take a const ptr?
-    init.key.bit_stream = const_cast<uint8_t*>(AES_128_KEY);
-    init.mode_params.gcm_info.tag_len = 16;
-    init.mode_params.gcm_info.nonce_len = 4;
-    init.flags = CAP_RAW_KEY | CAP_SYNC_OPS | CAP_SEPARATE_IO_BUFS;
-
-    cipher_pkt decryption_config;
-    decryption_config.in_buf = &buffer[ENCRYPTION_START_INDEX];
-    decryption_config.in_len = ENCRYPTED_DATA_SIZE;
-    decryption_config.out_buf_max = ENCRYPTED_DATA_SIZE;
-    decryption_config.out_buf = &buffer[ENCRYPTION_START_INDEX]; // Encrypt in place?
-
-    cipher_aead_pkt gcm_options;
-    gcm_options.ad = nullptr;
-    gcm_options.ad_len = 0;
-    gcm_options.pkt = &decryption_config;
-    gcm_options.tag = &buffer[26 - 16];
-
-    if (cipher_begin_session(crypto_device, &init, CRYPTO_CIPHER_ALGO_AES, CRYPTO_CIPHER_MODE_GCM, CRYPTO_CIPHER_OP_DECRYPT)) {
-        LOG_ERR("%s::ERROR - Cannot begin session", __PRETTY_FUNCTION__);
-        cipher_free_session(crypto_device, &init);
-        return false;
+    switch (command) {
+    case Command::LOCK_DOORS:
+        blink_pin(lock_doors_device, lock_doors_pin);
+        break;
+    case Command::UNLOCK_DOORS:
+        blink_pin(unlock_doors_device, unlock_doors_pin);
+        break;
+    case Command::START_CAR:
+        blink_pin(start_car_device, start_car_pin);
+        break;
+    default:
+        LOG_ERR("Invalid Command code passed to %s, Code = %u", __PRETTY_FUNCTION__, static_cast<uint8_t>(command));
+        break;
     }
-
-    if (cipher_gcm_op(&init, &gcm_options, reinterpret_cast<uint8_t*>(found_iv))) {
-        LOG_ERR("%s::ERROR: - Configuration/Input Error inside of decryption subroutine");
-        cipher_free_session(crypto_device, &init);
-        return false;
-    }
-
-    cipher_free_session(crypto_device, &init);
-    return true;
 }
-void write_iv_and_rolling_code_to_flash()
+
+MessageParameters get_initial_message_parameters()
 {
 
-    uint8_t flash_write_buffer[4];
+    // in lieu of having a constexpr max() function,
+    // to use the size of whichever is larger (IV_t or RollingCode_t),
+    // setting this to 8 bytes is safe enough.
+    uint8_t flash_read_buffer[8];
+    memset(flash_read_buffer, 0, sizeof(flash_read_buffer));
 
-    // Write IV to disk
-    memset(flash_write_buffer, 0, sizeof(flash_write_buffer));
-    memcpy(flash_write_buffer, &iv, sizeof(iv));
+    MessageParameters params;
 
-    auto flash_write_rc = nvs_write(&filesystem, IV_NVS_ID, flash_write_buffer, IV_NVS_SIZE);
-    if (flash_write_rc < 0) {
-        LOG_ERR("%s::ERROR: Disk write failure", __PRETTY_FUNCTION__);
+    // Read IV from disk
+    auto flash_read_rc = nvs_read(&filesystem, IV_NVS_ID, &flash_read_buffer, sizeof(IV_t));
+
+    if (flash_read_rc > 0) {
+        // IV is stored
+        memcpy(&params.iv, flash_read_buffer, sizeof(IV_t));
+    } else {
+        // IV is not stored
+        params.iv = IV_DEFAULT_VALUE;
     }
 
-    // Write Rolling Code to disk
-    memset(flash_write_buffer, 0, sizeof(flash_write_buffer));
-    memcpy(flash_write_buffer, &rolling_code, sizeof(rolling_code));
+    memset(flash_read_buffer, 0, sizeof(flash_read_buffer));
 
-    flash_write_rc = nvs_write(&filesystem, ROLLING_CODE_NVS_ID, flash_write_buffer, ROLLING_CODE_NVS_SIZE);
+    flash_read_rc = nvs_read(&filesystem, ROLLING_CODE_NVS_ID, &flash_read_buffer, sizeof(RollingCode_t));
+
+    if (flash_read_rc > 0) {
+        // Rolling Code is stored
+        memcpy(&params.rolling_code, flash_read_buffer, sizeof(RollingCode_t));
+    } else {
+        // Rolling code is not stored
+        params.rolling_code = ROLLING_CODE_DEFAULT_VALUE;
+    }
+
+    return params;
+}
+
+MessageParameters update_saved_message_params(const MessageParameters& current_params)
+{
+    uint8_t flash_write_buffer[8];
+
+    memset(flash_write_buffer, 0, sizeof(flash_write_buffer));
+    memcpy(flash_write_buffer, &current_params.iv, sizeof(IV_t));
+
+    auto flash_write_rc = nvs_write(&filesystem, IV_NVS_ID, flash_write_buffer, sizeof(IV_t));
+
     if (flash_write_rc < 0) {
-        LOG_ERR("%s::ERROR: Disk write failure", __PRETTY_FUNCTION__);
+        LOG_ERR("Failed to write IV to disk!");
+    }
+
+    memset(flash_write_buffer, 0, sizeof(flash_write_buffer));
+    memcpy(flash_write_buffer, &current_params.rolling_code, sizeof(RollingCode_t));
+
+    flash_write_rc = nvs_write(&filesystem, ROLLING_CODE_NVS_ID, flash_write_buffer, sizeof(RollingCode_t));
+
+    if (flash_write_rc < 0) {
+        LOG_ERR("Failed to write rolling code to disk!");
+    }
+
+    return current_params;
+}
+
+void handle_lora_message(uint8_t* message_buffer, uint16_t message_size, MessageParameters& params)
+{
+    if (message_buffer[0] != MARKER_BYTE || message_size != ENCRYPTION_BUFFER_SIZE) {
+        return;
+    }
+
+    uint8_t iv_buffer[Crypto::AES_GCM_IV_BUFFER_SIZE];
+    memset(iv_buffer, 0, sizeof(iv_buffer));
+    memcpy(iv_buffer, &message_buffer[IV_INDEX], sizeof(IV_t));
+
+    bool did_decrypt = Crypto::crypto_aes_gcm_decrypt(
+        &message_buffer[ENCRYPTION_START_INDEX],
+        &message_buffer[ENCRYPTION_START_INDEX],
+        &message_buffer[ENCRYPTION_TAG_START_INDEX],
+        ENCRYPTED_DATA_SIZE,
+        iv_buffer);
+
+    if (!did_decrypt) {
+        LOG_INF("%s::INFO: Received an invalid, but valid looking, request", __PRETTY_FUNCTION__);
+        return;
+    }
+
+    RollingCode_t given_rolling_code = 0;
+    memcpy(&given_rolling_code, &message_buffer[ROLLING_CODE_INDEX], sizeof(RollingCode_t));
+
+    // Now, this should ideally be aware of integer overflow, and account for that.
+    // Since overflowing is more than possible here.
+    // But, for now, converting it into a u64 works just as well.
+
+    uint64_t given_rolling_code_u64 = given_rolling_code;
+
+    if (given_rolling_code_u64 > params.rolling_code && given_rolling_code_u64 < (given_rolling_code_u64 + ROLLING_CODE_MAX_RANGE)) {
+        // The rolling code is valid!
+        memcpy(&params.iv, iv_buffer, sizeof(IV_t));
+        params.rolling_code = given_rolling_code;
+
+        update_saved_message_params(params);
+
+        auto command_to_run = static_cast<Command>(message_buffer[COMMAND_CODE_INDEX]);
+
+        handle_command(command_to_run);
     }
 }
 
@@ -135,6 +199,23 @@ bool init_lora()
         LOG_ERR("%s::ERROR Invalid LoRa configuration", __PRETTY_FUNCTION__);
         return false;
     }
+
+    return true;
+}
+
+bool init_gpio()
+{
+
+    bool ret = true;
+
+    lock_doors_device = device_get_binding("GPIOB");
+    ret &= gpio_pin_configure(lock_doors_device, 5, GPIO_OUTPUT);
+
+    unlock_doors_device = device_get_binding("GPIOA");
+    ret &= gpio_pin_configure(lock_doors_device, 9, GPIO_OUTPUT);
+
+    start_car_device = device_get_binding("GPIOB");
+    ret &= gpio_pin_configure(lock_doors_device, 10, GPIO_OUTPUT);
 
     return true;
 }
@@ -175,116 +256,11 @@ bool init_flash()
     return true;
 }
 
-void read_iv_and_rolling_code_from_flash()
-{
-
-    // nvs_read rc:
-    // >0: Bytes read successfully into the given buffer.
-
-    uint8_t flash_read_buffer[4];
-
-    // Read IV
-    memset(flash_read_buffer, 0, sizeof(flash_read_buffer));
-
-    auto flash_read_rc = nvs_read(&filesystem, IV_NVS_ID, &flash_read_buffer, IV_NVS_SIZE);
-
-    if (flash_read_rc > 0) {
-        memcpy(&iv, flash_read_buffer, sizeof(iv));
-    } else {
-        iv = IV_DEFAULT_VALUE;
-    }
-
-    // Read rolling code
-    memset(flash_read_buffer, 0, sizeof(flash_read_buffer));
-
-    flash_read_rc = nvs_read(&filesystem, ROLLING_CODE_NVS_ID, &flash_read_buffer, ROLLING_CODE_NVS_SIZE);
-
-    if (flash_read_rc > 0) {
-        memcpy(&rolling_code, flash_read_buffer, sizeof(rolling_code));
-    } else {
-        rolling_code = ROLLING_CODE_DEFAULT_VALUE;
-    }
-}
-
-void handle_command(Command command)
-{
-    const auto blink_pin = [](const device* device, uint8_t pin) {
-        gpio_pin_set(device, pin, 1);
-        k_sleep(K_MSEC(20));
-        gpio_pin_set(device, pin, 0);
-    };
-
-    switch (command) {
-    case Command::LOCK_DOORS:
-        blink_pin(lock_doors_device, lock_doors_pin);
-        break;
-    case Command::UNLOCK_DOORS:
-        blink_pin(unlock_doors_device, unlock_doors_pin);
-        break;
-    case Command::START_CAR:
-        blink_pin(start_car_device, start_car_pin);
-        break;
-    }
-}
-
-void handle_lora_message(uint8_t* message_buffer, uint16_t message_size)
-{
-    if (message_buffer[0] != MARKER_BYTE || message_size != ENCRYPTION_BUFFER_SIZE) {
-        return;
-    }
-
-    IV_t given_iv = 0;
-    memcpy(&given_iv, &message_buffer[IV_INDEX], sizeof(IV_t));
-
-    if (!aes_gcm_decrypt(message_buffer, given_iv)) {
-        LOG_INF("%s::INFO: Received an invalid, but valid looking, request", __PRETTY_FUNCTION__);
-        return;
-    }
-
-    RollingCode_t given_rolling_code = 0;
-    memcpy(&given_rolling_code, &message_buffer[ROLLING_CODE_INDEX], sizeof(RollingCode_t));
-
-    // Now, this should ideally be aware of integer overflow, and account for that.
-    // Since overflowing is more than possible here.
-    // But, for now, converting it into a u64 works just as well.
-
-    uint64_t given_rolling_code_u64 = given_rolling_code;
-
-    if (given_rolling_code_u64 > rolling_code && given_rolling_code_u64 < (given_rolling_code_u64 + ROLLING_CODE_MAX_RANGE)) {
-        // The rolling code is valid!
-        iv = given_iv;
-        rolling_code = given_rolling_code;
-        write_iv_and_rolling_code_to_flash();
-
-        auto command_to_run = static_cast<Command>(message_buffer[COMMAND_CODE_INDEX]);
-
-        handle_command(command_to_run);
-    }
-}
-
-bool init_gpio()
-{
-
-    bool ret = true;
-
-    lock_doors_device = device_get_binding("GPIOB");
-    ret &= gpio_pin_configure(lock_doors_device, 5, GPIO_OUTPUT);
-
-    unlock_doors_device = device_get_binding("GPIOA");
-    ret &= gpio_pin_configure(lock_doors_device, 9, GPIO_OUTPUT);
-
-    start_car_device = device_get_binding("GPIOB");
-    ret &= gpio_pin_configure(lock_doors_device, 10, GPIO_OUTPUT);
-
-    return true;
-}
-
 void main()
 {
     init_lora();
     init_gpio();
     init_flash();
-    read_iv_and_rolling_code_from_flash();
 
     crypto_device = device_get_binding(CRYPTO_DRV_NAME);
 
@@ -292,6 +268,10 @@ void main()
         LOG_ERR("%s::ERROR Failed to get the bindings for the crypto device, did you forget to enable it in your prj.conf?", __PRETTY_FUNCTION__);
         return;
     }
+
+    Crypto::crypto_aes_gcm_init(AES_128_KEY, 4, crypto_device);
+
+    auto params = get_initial_message_parameters();
 
     uint8_t lora_receive_buffer[64];
     memset(lora_receive_buffer, 0, sizeof(lora_receive_buffer));
@@ -303,7 +283,7 @@ void main()
         auto receive_rc = lora_recv(lora_dev, lora_receive_buffer, 64, K_SECONDS(10), &rssi, &snr);
         if (receive_rc > 0) {
             // We got a message!
-            handle_lora_message(lora_receive_buffer, receive_rc);
+            handle_lora_message(lora_receive_buffer, receive_rc, params);
         }
     }
 }
