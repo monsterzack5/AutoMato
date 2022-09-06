434162f7087260f11e6af91ad78177305c3f4467
monsterzack5 <monsterzack5@gmail.com>
2022-09-06 00:20:29 -0400

    Util: Don't try and center any binary output

--- Common/Util/print_bin.h
+++ Common/Util/print_bin.h
@@ -13,7 +13,7 @@ inline void print_bin(T number)
 {
     auto number_of_bits = ((sizeof(T) * 8));
 #ifdef CANRED
-    fmt::print("{:^0{}b}", number, number_of_bits);
+    fmt::print("{:0{}b}", number, number_of_bits);
 #elif defined ARDUINO
     for (int16_t i = number_of_bits - 1; i != -1; i -= 1) {
         Serial.print(((number >> i) & 1));
